822ad5be2d2a64f6f37bf3d271c4b80687684bc7
rudio <denisrudik01@gmail.com>
2023-08-16 14:30:14 +0300

    first commit

---
+++ app/src/main/java/com/example/newsappcompose/presentation/onboarding/Page.kt
@@ -0,0 +1,29 @@
+package com.example.newsappcompose.presentation.onboarding
+
+import androidx.annotation.DrawableRes
+import com.example.newsappcompose.R
+
+data class Page(
+    val title: String,
+    val description: String,
+    @DrawableRes val image: Int
+)
+
+val pages = listOf(
+    Page(
+        title = "Welcome to News App",
+        description = "This is a news app that uses News API to get the latest news from different sources.",
+        image = R.drawable.onboarding1
+    ),
+    Page(
+        title = "Get the latest news",
+        description = "Get the latest news from different sources.",
+        image = R.drawable.onboarding2
+    ),
+    Page(
+        title = "Search for news",
+        description = "Search for news from different sources.",
+        image = R.drawable.onboarding3
+    ),
+)
+
---
+++ app/src/main/java/com/example/newsappcompose/presentation/utils/Dimens.kt
@@ -0,0 +1,10 @@
+package com.example.newsappcompose.presentation.utils
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val MediumPadding1 = 24.dp
+    val MediumPadding2 = 28.dp
+
+    val IndicatorSize = 16.dp
+}
