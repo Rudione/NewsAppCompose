e959f3948aaf59230fbd8e9a1d5c9fdfa1062255
rudio <denisrudik01@gmail.com>
2023-08-23 10:46:08 +0300

    Features: add main viewmodel, navgraph

--- app/src/main/java/com/example/newsappcompose/presentation/MainActivity.kt
+++ app/src/main/java/com/example/newsappcompose/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,34 +16,34 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.lifecycleScope
 import com.example.newsappcompose.domain.usecases.AppEntryUseCases
 import com.example.newsappcompose.presentation.onboarding.OnBoardingScreen
+import com.example.newsappcompose.presentation.onboarding.OnBoardingViewModel
 import com.example.newsappcompose.ui.theme.NewsAppComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var appEntryUseCases: AppEntryUseCases
+    val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        installSplashScreen()
-
-        lifecycleScope.launch {
-            appEntryUseCases.readAppEntryUseCase.apply {
-                Log.d("MainActivity", "onCreate: $this")
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.splashCondition.value
             }
         }
 
         setContent {
             NewsAppComposeTheme {
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
-                    OnBoardingScreen()
+                    val startDestination = viewModel.startDestination
                 }
             }
         }
---
+++ app/src/main/java/com/example/newsappcompose/presentation/navgraph/Route.kt
@@ -0,0 +1,14 @@
+package com.example.newsappcompose.presentation.navgraph
+
+sealed class Route(
+    val route: String
+) {
+    object OnBoardingScreen : Route("onBoardingScreen")
+    object HomeScreen : Route("homeScreen")
+    object SearchScreen : Route("searchScreen")
+    object BookmarksScreen : Route("bookmarksScreen")
+    object DetailsScreen : Route("detailsScreen")
+    object AppStartNavigation : Route("appStartNavigation")
+    object NewsNavigation : Route("newsNavigation")
+    object NewsNavigatorScreen : Route("newsNavigatorScreen")
+}
