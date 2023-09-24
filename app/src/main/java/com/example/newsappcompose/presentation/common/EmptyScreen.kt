d9dd8bab5a5f803c5bc3bc45e557e11db10d50e3
rudio <denisrudik01@gmail.com>
2023-09-24 19:39:35 +0300

    Features: add screen when user haven't connect to network

--- app/src/main/java/com/example/newsappcompose/presentation/common/EmptyScreen.kt
+++ app/src/main/java/com/example/newsappcompose/presentation/common/EmptyScreen.kt
@@ -2,12 +2,27 @@ package com.example.newsappcompose.presentation.common
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import com.example.newsappcompose.R
 import java.net.ConnectException
@@ -35,16 +50,48 @@ fun EmptyScreen(error: LoadState.Error? = null) {
 
     val alphaAnimation by animateFloatAsState(
         targetValue = if (startAnimation) 0.3f else 0f,
-        animationSpec = tween(durationMillis = 1000), label = ""
+        animationSpec = tween(durationMillis = 1000)
     )
 
     LaunchedEffect(key1 = true) {
         startAnimation = true
     }
 
+    EmptyContent(
+        alphaAnim = alphaAnimation,
+        message = message,
+        iconId = icon
+    )
+
 }
 
-fun parseErrorMessage(error: LoadState.Error?): Any {
+@Composable
+fun EmptyContent(alphaAnim: Float, message: String, iconId: Int) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            painter = painterResource(id = iconId),
+            contentDescription = null,
+            tint = if (isSystemInDarkTheme()) Color.LightGray else Color.DarkGray,
+            modifier = Modifier
+                .size(120.dp)
+                .alpha(alphaAnim)
+        )
+        Text(
+            modifier = Modifier
+                .padding(10.dp)
+                .alpha(alphaAnim),
+            text = message,
+            style = MaterialTheme.typography.bodyMedium,
+            color = if (isSystemInDarkTheme()) Color.LightGray else Color.DarkGray
+        )
+    }
+}
+
+fun parseErrorMessage(error: LoadState.Error?): String {
     return when (error?.error) {
         is SocketTimeoutException -> "Server Unavailable"
         is ConnectException -> "Internet Unavailable"
