f1e0111e3237353434cb947755cf0c79f3112e65
rudio <denisrudik01@gmail.com>
2023-09-26 21:47:58 +0300

    Features: add details screen

---
+++ app/src/main/java/com/example/newsappcompose/presentation/screens/details/DetailsEvent.kt
@@ -0,0 +1,7 @@
+package com.example.newsappcompose.presentation.screens.details
+
+sealed class DetailsEvent {
+
+    object SaveArticle: DetailsEvent()
+
+}
---
+++ app/src/main/java/com/example/newsappcompose/presentation/screens/details/DetailsScreen.kt
@@ -0,0 +1,100 @@
+package com.example.newsappcompose.presentation.screens.details
+
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.newsappcompose.R
+import com.example.newsappcompose.domain.model.Article
+import com.example.newsappcompose.presentation.utils.Dimens.ArticleImageHeight
+
+@Composable
+fun DetailsScreen(
+    article: Article,
+    navigate: () -> Unit,
+    event: (DetailsEvent) -> Unit = {}
+) {
+
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+    ) {
+        DetailsTopBar(
+            onBrowsingClick = {
+                Intent(Intent.ACTION_VIEW).also {
+                    it.data = Uri.parse(article.url)
+                    if (it.resolveActivity(context.packageManager) != null) {
+                        context.startActivity(it)
+                    }
+                }
+            },
+            onShareClick = {
+                Intent(Intent.ACTION_SEND).also {
+                    it.putExtra(Intent.EXTRA_TEXT, article.url)
+                    it.type = "text/plain"
+                    if (it.resolveActivity(context.packageManager) != null) {
+                        context.startActivity(it)
+                    }
+                }
+            },
+            onBookmarkClick = { event(DetailsEvent.SaveArticle) },
+            onBackClick = navigate
+        )
+
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(
+                start = 16.dp,
+                end = 16.dp,
+                top = 16.dp
+            )
+        ) {
+            item {
+                AsyncImage(
+                    model = ImageRequest.Builder(context = context).data(article.urlToImage)
+                        .build(),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(ArticleImageHeight)
+                        .clip(MaterialTheme.shapes.medium),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(
+                    text = article.title,
+                    style = MaterialTheme.typography.displaySmall,
+                    color = colorResource(id = R.color.text_title)
+                )
+
+                Text(
+                    text = article.content,
+                    style = MaterialTheme.typography.displaySmall,
+                    color = colorResource(id = R.color.body)
+                )
+            }
+        }
+    }
+}
